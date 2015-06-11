9ddcc083182f929d1b060aa0cfcc41c2aa9350c0
Andrea Stagi <stagi.andrea@gmail.com>
2015-06-11 15:42:32 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+extra_kwargs = {}
+if sys.version_info >= (3,):
+    extra_kwargs['setup_requires'] = ['setuptools']
+
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+]
+
+setup(
+    name="lauda",
+    version="0.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A very simple python module for measuring time.",
+    license="MIT",
+    author="Andrea Stagi",
+    author_email="stagi.andrea@gmail.com",
+    url="",
+    keywords="time measurement",
+    install_requires=[
+    ],
+    classifiers=classifiers,
+    zip_safe=False,
+    **extra_kwargs)
